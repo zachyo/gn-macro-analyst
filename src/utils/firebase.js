fad6ba2067e811b1622dab306e0ca54dcbf2f354
zacheusIsrael <segunzacheusi@gmail.com>
2024-05-25 00:54:18 +0100

    redeploy

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -9,7 +9,6 @@ import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // apiKey: "AIzaSyADQ5EGqy2S43fqeZn4Ulq6u56udSa0jhI",
   apiKey : import.meta.env.VITE_APP_FIREBASE_API_KEY,
   authDomain: "gnmacroanalyst-gnappllc.firebaseapp.com",
   projectId: "gnmacroanalyst-gnappllc",
